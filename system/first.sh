86bec4c2c62d4bfe2598c8a945be15e13fba6675
Łukasz kawak Kawczyński <kawaczek@gmail.com>
2020-01-22 17:09:45 +0100

    Update first.sh

--- system/first.sh
+++ system/first.sh
@@ -1,5 +1,5 @@
 sudo date -s "$(curl -s --head http://google.com | grep ^Date: | sed 's/Date: //g')"
 sudo apt update
 sudo apt upgrade -y
-sudo apt install git mc python-pip screen-y 
+sudo apt install git mc python-pip screen -y 
 git clone --recursive https://github.com/kawaczek/pi0w

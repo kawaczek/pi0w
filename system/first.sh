0baaf74945296d4d572c3a3092a6b27e26fb2a47
Łukasz kawak Kawczyński <kawaczek@gmail.com>
2020-01-25 13:43:01 +0100

    Update first.sh

--- system/first.sh
+++ system/first.sh
@@ -1,4 +1,5 @@
 sudo date -s "$(curl -s --head http://google.com | grep ^Date: | sed 's/Date: //g')"
+sudo timedatectl set-timezone Europe/Warsaw
 sudo apt update
 sudo apt upgrade -y
 sudo apt install git mc python-pip screen -y 
